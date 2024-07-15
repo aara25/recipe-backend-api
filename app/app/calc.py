a1804fb0a88110592e428ebaf3ed5d71b95b7ef1
aara25 <aarathisekar2505@gmail.com>
2024-07-15 18:06:15 +0530

    Added noqa

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,6 +6,7 @@ def add(x,y): # noqa
     """Add x and y and return result"""
     return x + y
 
+
 def sub(x,y):
     """Subtract x and y and return result"""
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -7,15 +7,15 @@ from django.test import SimpleTestCase # noqa
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5,6)
         self.assertEqual(res,11)
-        
+       
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
         res = calc.sub(15,10)
         self.assertEqual(res,5)
-        
